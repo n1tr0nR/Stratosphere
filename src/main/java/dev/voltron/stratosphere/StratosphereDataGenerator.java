7afe61951842a1940a035c8e438df309c2247d2f
n1tr0n__ <166323600+n1tr0nR@users.noreply.github.com>
2025-11-18 16:35:14 +0000

    Initial commit

---
+++ src/main/java/dev/voltron/stratosphere/StratosphereDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.voltron.stratosphere;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StratosphereDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
