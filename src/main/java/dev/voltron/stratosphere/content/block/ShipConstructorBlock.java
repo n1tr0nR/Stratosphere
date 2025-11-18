ba20785667b02f771700a1ceeb5df930b09d30ef
n1tr0n__ <166323600+n1tr0nR@users.noreply.github.com>
2025-11-18 16:36:50 +0000

    Basic stuffs

---
+++ src/main/java/dev/voltron/stratosphere/content/block/ShipConstructorBlock.java
@@ -0,0 +1,4 @@
+package dev.voltron.stratosphere.content.block;
+
+public class ShipConstructorBlock {
+}
---
+++ src/main/java/dev/voltron/stratosphere/content/entity/ShipEntity.java
@@ -0,0 +1,4 @@
+package dev.voltron.stratosphere.content.entity;
+
+public class ShipEntity {
+}
